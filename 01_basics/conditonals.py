081c51c3fd248264e65df817531c5eee38fb5f38
MashyBasker <basumaharshi10@gmail.com>
2023-01-27 17:46:23 +0530

    Add loops and if

---
+++ 01_basics/conditonals.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+if a < b :
+    print("a is smaller")
+else:
+    print("b is smaller")
+
+
+k = -21
+if k > 0:
+    print("Positive")
+elif k < 0:
+    print("Negative")
+else:
+    print("Zero")
---
+++ 01_basics/loops.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    print(i, end=" ")
+print('\n')
+
+j = 100
+while j > 0:
+    print(j)
+    j -= 1
+
